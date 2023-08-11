8ba1f9839d9f0006bc16bafb7e8a6c28d30ce8d4
Ankit Kumar <ankitkr97y@gmail.comr>
2023-08-11 15:25:03 +0530

    practice variable datatype

--- practice.js
+++ practice.js
@@ -1,1 +1,74 @@
-console.log('hello world');
+// console.log('hello world')
+// ********************* Variable Part Learning ************************************
+// write a variable on javascript provide three keywods
+// 1. let 2. var 3. const
+
+// let age; // declatarion
+// age = 20; //assignment
+
+// let num = 10; // delcatarion and assignment
+// let teacherName; //camel case
+// let teacher_name; // snake case
+
+// console.log(typeof num)
+// const name; // worng method in constant
+
+// const fatherName = "Naresh Mandal";
+// // console.log(fatherName)
+// fatherName = "PANKAJ KUMAR"
+// console.log(fatherName)
+
+// ************************** DataType *************************************************
+
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null
+// 5. Undefiend
+// 6. NaN
+// 7. Object
+// 8. Symbole
+
+// Example of data Type......................
+// 1. Nubmber 
+// let num= 10;
+// let num2= 10.5;
+// let num3 = 10.555;
+
+// console.log(num)
+// console.log(num2)
+// console.log(num3)
+
+// console.log(typeof num)
+// console.log(typeof num2)
+// console.log(typeof num3)
+
+
+// 2. String Data Type ........................
+
+// const name = 'ankit';
+// console.log(typeof name)
+
+// 3. Boolean Data Type .......................
+
+// let trueName = true;
+// console.log(typeof trueName)
+
+// let num1=2;
+// let num2=2;
+
+// let comparision = num1<=num2;
+// console.log(comparision)
+// console.log(typeof comparision)
+
+// 4. Null Data Type ..........................
+//  let number = null;
+//  console.log(typeof number)
+
+// 5. Undefiend data Type ......................
+let num;
+console.log(typeof num);
+
+// 6. NaN data Type ...........................
+let reslt = "I am string"/2;
+console.log(reslt)
